b9cff5d0ac718977b12d5bc78cd18d7fd13a192a
MatrixCodeDE <matr.xonyt@gmail.com>
2024-05-03 02:55:54 +0200

    Created easy input fields for GuiChat via array

--- src/main/java/de/matrix/pvnikamod/gui/GuiBase.java
+++ src/main/java/de/matrix/pvnikamod/gui/GuiBase.java
@@ -3,7 +3,9 @@ package de.matrix.pvnikamod.gui;
 import de.matrix.pvnikamod.config.Config;
 import de.matrix.pvnikamod.main.PvnikaMod;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
 
 import java.io.IOException;
 
@@ -13,6 +15,7 @@ public abstract class GuiBase extends GuiScreen {
     public final Config config;
     public final Minecraft mc;
     public final GuiScreen lastScreen;
+    protected GuiButton backButton;
 
     public GuiBase(GuiScreen lastScreen) {
         this.mod = PvnikaMod.getInstance();
@@ -25,13 +28,37 @@ public abstract class GuiBase extends GuiScreen {
         super.initGui();
     }
 
+    public void setBackButton(int y) {
+        setBackButton(width / 2 - 30, y);
+    }
+
+    public void setBackButton(int x, int y){
+        setBackButton(x, y, 60, 20);
+    }
+
+    public void setBackButton(int x, int y, int widthIn, int heightIn){
+        buttonList.add(this.backButton = new GuiButton(10, x, y, widthIn, heightIn, I18n.format("gui.back")));
+    }
+
+    public void drawScreenNoBG(int mouseX, int mouseY, float partialTicks){
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     public void refreshButtons() {
 
+    }
+
+    @Override
+    public void actionPerformed(GuiButton button) throws IOException {
+        if (button.id == 10){
+            this.mc.displayGuiScreen(lastScreen);
+        }
     }
 
     @Override
@@ -42,7 +69,11 @@ public abstract class GuiBase extends GuiScreen {
 
 
     @Override
-    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {}
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
     }
 
 }
--- src/main/java/de/matrix/pvnikamod/gui/GuiChat.java
+++ src/main/java/de/matrix/pvnikamod/gui/GuiChat.java
@@ -7,17 +7,14 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiListButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.resources.I18n;
 
 import java.io.IOException;
 
 public class GuiChat extends GuiBase {
 
-    private GuiTextField text1;
-    private GuiButton ctrl1;
-    private GuiTextField text2;
-    private GuiButton ctrl2;
-    private GuiTextField text3;
-    private GuiButton ctrl3;
+    private final GuiTextField[] texts = new GuiTextField[5];
+    private final GuiButton[] controls = new GuiButton[5];
 
 
     public GuiChat(GuiScreen lastScreen) {
@@ -29,20 +26,53 @@ public class GuiChat extends GuiBase {
         super.initGui();
         int i = -2;
         int j = 24;
-        this.text1 = new GuiTextField(0, fontRendererObj, width / 2 - 50, height / 4 + 0 + i, 100, 20);
-        buttonList.add(this.ctrl1 = new GuiButton(1, width / 2 - 50, height / 4 + j + i, 100, 20, "Ctrl"));
+        for (int cnt = 0; cnt < texts.length; cnt++) {
+            this.texts[cnt] = new GuiTextField(0, fontRendererObj, width / 2 - 115, height / 4 + cnt * j + i, 160, 20);
+            buttonList.add(this.controls[cnt] = new GuiButton(cnt, width / 2 + 50, height / 4 + cnt * j + i, 60, 20, I18n.format("menu.pvnika.chat.off")));
+        }
+        setBackButton(height / 4 + texts.length * j + i);
+        refreshButtons();
     }
 
+    @Override
+    public void refreshButtons() {
 
+        super.refreshButtons();
+    }
+
+    @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
+        for (GuiTextField tF : texts) {
+            tF.drawTextBox();
+        }
     }
 
+    @Override
+    public void updateScreen() {
+        for (GuiTextField tF : texts) {
+            tF.updateCursorCounter();
+        }
+    }
 
+    @Override
     protected void keyTyped(char typedChar, int keyCode) throws IOException {
-        text1.textboxKeyTyped(typedChar, keyCode);
+        for (GuiTextField tF : texts) {
+            tF.textboxKeyTyped(typedChar, keyCode);
+        }
         super.keyTyped(typedChar, keyCode);
     }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        for (GuiTextField tF : texts) {
+            tF.mouseClicked(mouseX, mouseY, mouseButton);
+        }
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+
+
 
 
 }
